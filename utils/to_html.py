2fdc9ceeae30783d8e0777e33d7a744eca40f244
Oliver Moeller <omoeller@verify-it.de>
2024-03-29 15:24:45 +0100

    added conversion to HTML

---
+++ utils/to_html.py
@@ -0,0 +1,82 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+import random
+
+info = sys.argv[1]
+txt = sys.argv[2:]
+
+random.seed(4711)
+
+def c2html(c):
+    if c in "öäüÖÄÜß&\n<>":
+        if c == 'ö':
+            return "&ouml;"
+        if c == 'ä':
+            return "&auml;"
+        if c == 'ü':
+            return "&uuml;"
+        if c == 'Ö':
+            return "&Ouml;"
+        if c == 'Ä':
+            return "&Auml;"
+        if c == 'Ü':
+            return "&Uuml;"
+        if c == 'ß':
+            return "&szlig;"
+        if c == "&":
+            return "&amp;"
+        if c == "\n":
+            return "<br>"
+        if c == "<":
+            return "&lt;"
+        if c == ">":
+            return "&gt;"
+    return c
+
+def txt2html(txt):
+    return "".join([c2html(x) for x in txt])
+
+def next_col():
+    return  random.randint(0, 0xFFFFffff)
+
+pat_ref = re.compile('^([^ =]+)[=]([^,]+)[,].*')
+
+ref = {}
+with open(info, 'r') as inf:
+    for line in inf:
+        m = pat_ref.match(line)
+        if m is not None:
+            ref[txt2html(m.group(1))] = '<B><FONT COLOR="#{0:08x}">{1}</FONT></B>'.format(next_col(), txt2html(m.group(2)))
+keys = list(ref.keys())
+            
+#sys.stderr.write("USING Mapping: {0}".format(ref))
+
+print("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>Selters - mit den Klons kamen die Tr&auml;nen (Der sch&ouml;nste Film der Welt)</TITLE>
+</HEAD>
+<BODY>
+""")
+
+for f in txt:
+    print("<H1>{0}</H1>".format(txt2html(f)))
+    with open(f, 'r') as inf:
+        for line in inf:
+            if '#' == line[0]:
+                #print("<!-- {0} -->".format(txt2html(line).replace('<br>', '')))
+                continue
+            line = txt2html(line)
+            for k in keys:
+                line = line.replace(k, ref[k])
+            print(line)
+print("""
+</BODY>
+</HTML>
+""")            
+          
+    
+
